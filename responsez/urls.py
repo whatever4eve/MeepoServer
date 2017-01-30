83a468a274ad1a9ccf04bd6b955511781f09ec73
bar azaria <azariaba@gmail.com>
2017-01-30 22:04:14 +0200

    extended the user model

--- responsez/urls.py
+++ responsez/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^signup/',views.signup,name='signup'),
     url(r'^signin/',tviews.obtain_auth_token,name='signin'),
+    url(r'^myprofile/',views.myprofile,name='profile'),
 ]
--- responsez/views.py
+++ responsez/views.py
@@ -3,8 +3,15 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.db import IntegrityError
+from django.utils import timezone
 from json import dumps
+from .models import UserProfile
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import api_view,authentication_classes,permission_classes
 # Create your views here.
+
+
 
 
 def index(request):
@@ -20,14 +27,23 @@ def signup(request):
     try:
     	user = User.objects.create_user(username,email,password)
     	a['status'] = "success"
+        profile = UserProfile(user=user,city="jers",birthdate=timezone.now(),bio="anal sex")
+        profile.save()
     except IntegrityError:
     	a['status'] = "userexists"
     	a['user'] = username
-    except:
-    	a['status'] = "wrong"
+    #except:
+    #	a['status'] = "wrong"
 
     return HttpResponse(dumps(a))
-    
-@csrf_exempt    
-def signin(request):
-    return HttpResponse("123")
+
+
+@api_view(['GET'])
+@authentication_classes((TokenAuthentication,))
+@permission_classes((IsAuthenticated,))
+def myprofile(request):
+    data = {}
+    data['username'] = request.user.username
+    data['name'] = request.user.first_name + ' ' +request.user.last_name
+    return HttpResponse(dumps(data))
+
